0777bd525cac8076861ace98ae45a53407a0aa36
Dani Acherkan <dtl.117@gmail.com>
2014-05-26 00:26:09 +0300

    Match model

--- db/schema.rb
+++ db/schema.rb
@@ -11,10 +11,27 @@
 #
 # It's strongly recommended that you check this file into your version control system.
 
-ActiveRecord::Schema.define(version: 20140523175449) do
+ActiveRecord::Schema.define(version: 20140525212431) do
 
   # These are extensions that must be enabled in order to support this database
   enable_extension "plpgsql"
+  enable_extension "hstore"
+
+  create_table "matches", force: true do |t|
+    t.integer  "team_a"
+    t.integer  "team_b"
+    t.string   "round"
+    t.datetime "starting_time"
+    t.datetime "created_at"
+    t.datetime "updated_at"
+    t.hstore   "score"
+  end
+
+  create_table "teams", force: true do |t|
+    t.string   "code"
+    t.datetime "created_at"
+    t.datetime "updated_at"
+  end
 
   create_table "users", force: true do |t|
     t.string   "email",                              default: "", null: false

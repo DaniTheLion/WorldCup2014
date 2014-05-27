d7bc2dfa38dea2bf7b492758deb518e254eb4d0e
Dani Acherkan <dtl.117@gmail.com>
2014-05-28 00:54:17 +0300

    dont force ssl for heroku

---
+++ db/migrate/20140527211232_create_matches.rb
@@ -0,0 +1,10 @@
+class CreateMatches < ActiveRecord::Migration
+  def change
+    create_table :matches do |t|
+      t.integer :team_a_id
+      t.integer :team_b_id
+      t.string :round
+      t.datetime :starting_time
+    end
+  end
+end

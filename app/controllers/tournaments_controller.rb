01523a7ac3acb3e5a0c3e7789483f5325895f7f8
Dani Acherkan <dtl.117@gmail.com>
2014-06-17 00:29:14 +0300

    display latest past matches

--- app/controllers/tournaments_controller.rb
+++ app/controllers/tournaments_controller.rb
@@ -5,7 +5,7 @@ class TournamentsController < ApplicationController
 	def show
       # @next_matches = Match.where("starting_time > ?", Time.now).all
       @next_matches = Match.where("score is null").order('starting_time asc').first(10)
-      @past_matches = Match.completed.order('starting_time desc').last(10)
+      @past_matches = Match.completed.order('starting_time desc').first(10)
       @teams = Team.all
       @users = User.order('points desc').all
       respond_to do |format|
